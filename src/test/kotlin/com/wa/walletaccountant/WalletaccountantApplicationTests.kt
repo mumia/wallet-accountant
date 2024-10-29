9a9501619a77febb38663c89ee07d34b16222a09
mlouromanso <mlouromanso@wayfair.com>
2024-10-29 08:06:43 +0100

    Add base structure

---
+++ src/test/kotlin/com/wa/walletaccountant/WalletaccountantApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wa.walletaccountant
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WalletaccountantApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
