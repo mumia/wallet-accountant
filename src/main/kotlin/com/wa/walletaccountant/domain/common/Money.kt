6cfdd41cd79bec630da3ab1e3ada764155825da0
mlouromanso <mlouromanso@wayfair.com>
2024-11-26 18:00:05 +0100

    Add equals and hashCode to Money

--- src/main/kotlin/com/wa/walletaccountant/domain/common/Money.kt
+++ src/main/kotlin/com/wa/walletaccountant/domain/common/Money.kt
@@ -5,4 +5,21 @@ data class Money(
     val currency: Currency,
 ) {
     override fun toString(): String = "%.2f %s".format(value, currency.toString())
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Money
+
+        return value == other.value &&
+            currency == other.currency
+    }
+
+    override fun hashCode(): Int {
+        var result = value.hashCode()
+        result = 31 * result + currency.hashCode()
+
+        return result
+    }
 }
