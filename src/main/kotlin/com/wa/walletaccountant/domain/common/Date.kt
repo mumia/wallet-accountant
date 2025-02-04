5d6f179b2f6ca0c439a0f2b33604fed3344e2603
mlouromanso <mlouromanso@wayfair.com>
2025-02-04 17:46:18 +0100

    Base saga class and date improvements

--- src/main/kotlin/com/wa/walletaccountant/domain/common/Date.kt
+++ src/main/kotlin/com/wa/walletaccountant/domain/common/Date.kt
@@ -2,6 +2,8 @@ package com.wa.walletaccountant.domain.common
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonValue
+import java.time.Month
+import java.time.Year
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
@@ -9,9 +11,11 @@ import java.time.format.DateTimeFormatter.ISO_LOCAL_DATE
 import java.time.temporal.ChronoUnit
 
 data class Date(
-    val value: ZonedDateTime,
+    private val value: ZonedDateTime,
 ) {
     companion object {
+        val UTC: ZoneId = ZoneId.of("UTC")
+
         fun now(): Date = Date(ZonedDateTime.now().truncatedTo(ChronoUnit.DAYS))
 
         @JsonCreator
@@ -19,10 +23,23 @@ data class Date(
             Date(
                 ZonedDateTime.parse(
                     stringValue + "T00:00:00Z",
-                    DateTimeFormatter.ISO_INSTANT.withZone(ZoneId.of("UTC")),
+                    DateTimeFormatter.ISO_INSTANT.withZone(UTC),
                 ),
             )
+
+        fun nextMonth(month: Month, year: Year): Date =
+            Date(
+                ZonedDateTime
+                    .of(year.value, month.value, 1, 0, 0, 0, 0, UTC)
+                    .plusMonths(1)
+            )
     }
+
+    fun year(): Year = Year.of(value.year)
+
+    fun month(): Month = Month.of(value.monthValue)
+
+    fun day(): Int = value.dayOfMonth
 
     @JsonValue
     override fun toString(): String = value.format(ISO_LOCAL_DATE)
